d4fd9f3170df4a9adec907d5c603bcdc2f1db97e
Ke Chen <JingYiJun3104@outlook.com>
2024-02-25 10:47:09 +0800

    chore: update theme to new app name

--- app/src/androidTest/java/com/fduhole/danxi/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/fduhole/danxi/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.fduhole.danxinative", appContext.packageName)
+        assertEquals("com.fduhole.danxi", appContext.packageName)
     }
 }
